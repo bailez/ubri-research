504aa6994e57d1a3975991c909f39cb5bbc26e01
bailez <bailez@usp.br>
2021-10-10 10:08:35 -0300

    coinbase read ohlc

--- scripts/create_dataframe.py
+++ scripts/create_dataframe.py
@@ -1,12 +1,23 @@
 from coinbase import get_coin_series
+import pandas as pd
 from coins import COINS
 
 COINS
 
 begin = '2010-01-01'
 end = '2021-12-31'
-crypto = 'BTC'
 
-#
 
-df = get_coin_series(begin, end, crypto)
+# COINBASE
+
+writer = pd.ExcelWriter('coinbase.xlsx', engine='xlsxwriter')
+
+for coin in COINS:   
+
+    crypto = (COINS[coin])
+
+    df = get_coin_series(begin, end, crypto)
+
+    df.to_excel(writer, sheet_name = coin)
+
+writer.save()
